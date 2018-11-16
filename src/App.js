5487a95e0d04c6e63746257d1239b8c8450dae86
Maximilian Alexander <mbalex99@gmail.com>
2018-11-16 15:01:33 -0800

    putting in column

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import { Fade } from "react-slideshow-image";
 
 import './App.css';
@@ -10,8 +10,8 @@ const properties = {
   duration: 5000,
   transitionDuration: 500,
   infinite: true,
-  indicators: true,
-  arrows: true
+  indicators: false,
+  arrows: false
 }
 
 class App extends Component {
@@ -19,23 +19,25 @@ class App extends Component {
     return (
       <div>
         <Container>
-          <Row md={6}>
-            <h1>Lorem ipsum dolor sit amet</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula ipsum magna, quis laoreet purus rhoncus at. Vestibulum laoreet odio a neque tristique pharetra. Phasellus cursus odio ut nibh porttitor faucibus. Sed vehicula magna ut libero rhoncus, vitae molestie erat venenatis. Nam ultrices urna a ligula bibendum porttitor. Quisque efficitur ornare turpis, ac tempus urna elementum et. Suspendisse eleifend blandit scelerisque. In dignissim nulla a fermentum fermentum.</p>
-          </Row>
-          <Row md={6}>
-            <Fade {...properties}>
-              <div className="each-fade">
-                <div className="image-container">
-                  <img src={FIRST_IMG} />
+          <Row className="align-items-center">
+            <Col md={6}>
+              <h1>Lorem ipsum dolor sit amet</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula ipsum magna, quis laoreet purus rhoncus at. Vestibulum laoreet odio a neque tristique pharetra. Phasellus cursus odio ut nibh porttitor faucibus. Sed vehicula magna ut libero rhoncus, vitae molestie erat venenatis. Nam ultrices urna a ligula bibendum porttitor. Quisque efficitur ornare turpis, ac tempus urna elementum et. Suspendisse eleifend blandit scelerisque. In dignissim nulla a fermentum fermentum.</p>
+            </Col>
+            <Col md={6}>
+              <Fade {...properties}>
+                <div className="each-fade">
+                  <div className="image-container">
+                    <img src={FIRST_IMG} />
+                  </div>
                 </div>
-              </div>
-              <div className="each-fade">
-                <div className="image-container">
-                  <img src={SECOND_IMG} />
+                <div className="each-fade">
+                  <div className="image-container">
+                    <img src={SECOND_IMG} />
+                  </div>
                 </div>
-              </div>
-            </Fade>
+              </Fade>
+            </Col>
           </Row>
         </Container>
       </div>
